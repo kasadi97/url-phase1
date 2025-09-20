a308fd47f904f724afbe2609d1a3a4ebc5263a9d
Adilbek <adilbek.kass@gmail.com>
2025-09-20 13:23:58 +0500

    front added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { UrlModule } from './url/url.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { PrismaService } from './prisma/prisma.service';
-//import { RedirectController } from './redirect/redirect.controller';
+import { RedirectController } from './redirect/redirect.controller';
 import { RedirectModule } from './redirect/redirect.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [PrismaModule, UrlModule, RedirectModule, AuthModule],
-  //controllers: [RedirectController],
+  controllers: [RedirectController],
   providers: [ PrismaService],
 })
 export class AppModule {}
